40ff4573ba55031025d06ff5914f42471afbd3ed
devarshpatel1 <devarsh.patel1@dcmail.ca>
2025-01-17 13:28:54 -0500

    List

--- ice/client/app/animal.service.mock.js
+++ ice/client/app/animal.service.mock.js
@@ -9,34 +9,28 @@ function AnimalService() {
         localStorage.setItem('animals', JSON.stringify([]))
     }    
 }
-
 /*
  *
  */
 AnimalService.prototype.getAnimals = function() {
     // this will always be set, because we did it in the constructor
+    return JSON.parse(localStorage.getItem('animals'));
 }
 /*
  *
  */
 AnimalService.prototype.saveAnimal = function(animal) {
     // get a list of animals
-    const animals = this.getAnimals();
     //const animals = this.getAnimals();
     // see if this animal already exists
-    if (animals.find(a => a.name == animal.name)) {
     //if (animals.find(a => a.name == animal.name)) {
         // tell the caller we're not going to save this
-        throw new Error('An animal with that name already exists!');
-    }
         //throw new Error('An animal with that name already exists!');
     //}
     // if it doesn't, add it to the array
-
     let animals = JSON.parse(localStorage.getItem('animals'));
     animals.push(animal);
     // and save it in storage again
-
     localStorage.setItem('animals', JSON.stringify(animals));
     // tell the caller all was well
     return true;
@@ -45,26 +39,17 @@ AnimalService.prototype.saveAnimal = function(animal) {
  *
  */
 AnimalService.prototype.findAnimal = function(animalName) {
-    const animals = this.getAnimals();
-    const animal = animals.find(a => a.name == animalName);
-    if (!animal) {
-        throw new Error('That animal does not exist!');
-    }
-    return animal;
+    return null;
 }
+
 /*
  *
  */
 AnimalService.prototype.updateAnimal = function(animal) {
-    const animals = this.getAnimals();
-    const idx = animals.findIndex(a => a.name == animal.name);
-    if (idx === -1) {
-        throw new Error('That animal does not exist!');
-    }
-    animals[idx] = animal;
-    localStorage.setItem('animals', JSON.stringify(animals));
-    return true;
+
+    return false;
 }
+
 /*
  *
  */
@@ -78,4 +63,5 @@ AnimalService.prototype.deleteAnimal = function(animal) {
     localStorage.setItem('animals', JSON.stringify(animals));
     return true;
 }
+
 const animalService = new AnimalService();
