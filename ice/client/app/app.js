9b9dd63f3bbdacb28b533bcfb5f97b30376157c9
devarshpatel1 <devarsh.patel1@dcmail.ca>
2025-01-17 13:51:34 -0500

    Update

--- ice/client/app/app.js
+++
@@ -1,7 +0,0 @@
-/*
-    Name: Devarsh Patel
-    filename: app.js
-    Course: INFT 2202
-    Date: January 10,2025
-    Description: This is my general application script.  Functions that are required on every page should live here.
-*/
