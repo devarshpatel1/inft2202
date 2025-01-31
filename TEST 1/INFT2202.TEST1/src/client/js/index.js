54d0766de441cf9e24a4dc6518f99fc1228982c8
devarshpatel1 <devarsh.patel1@dcmail.ca>
2025-01-31 17:40:16 -0500

    Update Index.js
    -Update insertMoviesIntoTable(eleTable, moviesList function
    - convert the js formet to the proper english time stamp formet

--- TEST 1/INFT2202.TEST1/src/client/js/index.js
+++ TEST 1/INFT2202.TEST1/src/client/js/index.js
@@ -56,28 +56,70 @@ function getPinnedMoviesFromStorage() {
  *  - a list of movies to put in the table
  *  It should return nothing
  */
-function insertMoviesIntoTable(eleTable, movies) 
-{
+function insertMoviesIntoTable(eleTable, moviesList) {
     // sort the list of movies by rating, highest to lowest
-    // for each movie
-        // insert a row
-        // insert a cell for each attribute of a movie
-        // the datetime is a "unix timestamp", measured in seconds.  
-        //   javascript dates are measured in milliseconds.
-        //   convert this timestamp to a javascript date and print out the date as a normal string in english
-        // create a new button element
-        // look in local storage to see if this item is already pinned
-        //   if it's already pinned, make it red, otherwise make it blue
-        // set the html so it shows a font-awesome icon
-        //   if it's already pinned, show an x, otherwise show a pencil
-        // add an event listener, when this button is clicked...
-            // if it is, remove it from the list
-            // it it's not, add it to the list 
-            // refresh the page
+    // if this movie is a drama, don't add it to the list
+    let sortedMovies = moviesList.filter(m => m.genre !== "Drama").sort((a, b) => a.rating - b.rating);
+    let tbody = eleTable.querySelector("tbody");
+    tbody.innerHTML = "";
+    
+    sortedMovies.forEach(movie => {
+        let row = tbody.insertRow();
+        row.insertCell(0).textContent = movie.title;
+        row.insertCell(1).textContent = movie.genre;
+                // Convert release_date (in seconds) to a Date object
+                let releaseDate = new Date(movie.release_date * 1000);
+        
+                // Format release date as "yyyy-mm-dd , hh:mm:ss am/pm"
+                let options = {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    hour12: true, // AM/PM format
+                };
+                
+                let formattedDate = releaseDate.toLocaleString('en-US', options);
+                
+                // Split the formattedDate into the date and time parts
+                let [date, time] = formattedDate.split(', ');
+                
+                // Reformat the date into "yyyy-mm-dd" format
+                let [month, day, year] = date.split('/');
+                let formattedDateString = `${year}-${month}-${day}`;
+                
+                // Now format the final string: "yyyy-mm-dd , hh:mm:ss am/pm"
+                let finalDateTimeString = `${formattedDateString} , ${time}`;
+                
+                // Insert the formatted date and time into the table
+                row.insertCell(2).textContent = finalDateTimeString;
+        row.insertCell(3).textContent = movie.director;
+        row.insertCell(4).textContent = movie.rating;
+        
+        let btnCell = row.insertCell(5);
+        let btn = document.createElement("button");
+        let pinnedMovies = getPinnedMoviesFromStorage();
+        let isPinned = pinnedMovies.some(m => m.title === movie.title);
+        
+        btn.classList.add("btn", isPinned ? "btn-danger" : "btn-primary");
+        btn.innerHTML = `<i class="fa ${isPinned ? "fa-times" : "fa-pencil"}"></i>`;
+        btn.addEventListener("click", () => {
+            let updatedPinnedMovies = getPinnedMoviesFromStorage();
+            if (isPinned) {
+                updatedPinnedMovies = updatedPinnedMovies.filter(m => m.title !== movie.title);
+            } else {
+                updatedPinnedMovies.push(movie);
+            }
+            localStorage.setItem("pinnedMovies", JSON.stringify(updatedPinnedMovies));
+            location.reload();
+        });
+        btnCell.appendChild(btn);
         // create another table row and put the button in it
         // if a movie is rated two or below, make this row red
         // if this movie is rated higher than two but less than or equal to five, make this row orange
         // if this movie is rated higher than five but less than or equal to 8, make this row blue
         // if this movie is rated higher than eight, make this row green
-        // if this movie is a drama, don't add it to the list
+    });
 }
